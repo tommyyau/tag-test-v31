589d66786b25c79241f81182294f28aae2f848a3
liberty <platformservices@gmail.com>
2020-08-27 20:49:49 +0000

    Initial Commit

--- packages/beauty-portal-theme/src/helpers/sanityConfig.ts
+++ packages/beauty-portal-theme/src/helpers/sanityConfig.ts
@@ -1,4 +1,4 @@
 export const sanityConfig = {
-  projectId: 'PROJECT_ID',
-  dataset: 'DATASET_ID',
+  projectId: 'fign0p77',
+  dataset: 'production',
 };
--- packages/studio/src/dashboardConfig.js
+++ packages/studio/src/dashboardConfig.js
@@ -18,12 +18,12 @@ export default {
         data: [
           {
             title: 'GitHub repo',
-            value: 'REPO_URL',
+            value: 'https://github.com/tommyyau/tag-test-v31.git',
             category: 'Code'
           },
           {
             title: 'Frontend',
-            value: 'FRONTEND_URL',
+            value: 'http://unruffled-bohr-07a412.netlify.app',
             category: 'apps'
           }
         ]
